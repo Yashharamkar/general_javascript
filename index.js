416708ce419de626de4e5533d52f0da0aa84ce7d
Yashharamkar <87219809+Yashharamkar@users.noreply.github.com>
2022-10-04 13:49:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let count=0;
+const arr=["yash","naitik","laksh","mummy","papa"];
+
+let yash =document.getElementById("yash")
+ let name="yash";
+ let greeting="welcome baby";
+function onClick(){
+    count +=1;
+ yash.innerText = count;
+ yash1.innerText=arr[count];
+}
+
+
+let yash1=document.getElementById("yash1");
+let i=0;
+let len=arr.length;
+function save(){
+    
+    yash1.innerText="Yash the brand"; 
+   
+   
+ }
+
+
+
+
+
+
